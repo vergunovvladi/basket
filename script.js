2ef9629053ade7ee2ee1c466667c73739b7b4963
vladislavvergunov <vladislav.vergunov@icloud.com>
2021-06-17 19:04:14 +0300

    firstCommit

--- script.js
+++ script.js
@@ -5,14 +5,62 @@ const links = document.querySelector('.links'),
       inputProduct = document.querySelector('.header__product'),
       inputQuantity = document.querySelector('.header__quantity'),
       inputPrice = document.querySelector('.header__price'),
-      shoppingList = document.querySelector('.list__shopping'),
-      itemObj = {
-          item: '',
-          quantity: '',
-          price: ''
-      };
+      shoppingList = document.querySelector('.list__shopping');
 
-let counter = 1;
+let productArr = [];
+
+const addToLocalStorage = function() {
+    let itemObj = {
+        item: inputProduct.value,
+        quantity: inputQuantity.value,
+        price: inputPrice.value
+    };
+
+    productArr.push(itemObj);
+    localStorage.setItem('products', JSON.stringify(productArr));
+}
+
+const showShoppingList = function() {
+    let returnLocal = JSON.parse(localStorage.getItem('products'));
+    if(returnLocal !== null) {
+        shoppingList.innerHTML = '';
+        for(let i = 0; i < returnLocal.length; i++) {
+            newShoppingItem = document.createElement('li');
+            shoppingList.append(newShoppingItem);
+            newShoppingItem.className = 'list__shoppingItem';
+            newShoppingItem.innerHTML = `
+            <span class="list__number">${i+1}</span>
+            <span class="list__name">${returnLocal[i].item}</span>
+            <span class="list__quantity">${returnLocal[i].quantity}</span>
+            <span class="list__price">${returnLocal[i].price}</span>
+            <div class="list__button">
+                <button class="list__doneButton">V</button>
+                <button class="list__editButton">E</button>
+                <button class="list__deleteButton">X</button>
+            </div>
+            `;
+        }
+    }
+}
+
+
+showShoppingList();
+
+shoppingList.addEventListener('click', (event) => {
+    const deleteTarget = event.target.closest('li'),
+        deleteItem = deleteTarget.querySelector('.list__number');
+        let indexItem = parseInt(deleteItem.innerHTML);
+        deleteShopingItem(indexItem - 1);
+        showShoppingList();
+});
+
+
+const deleteShopingItem = function(index) {
+    let returnLocal = JSON.parse(localStorage.getItem('products'));
+    let deleteLocal = returnLocal.splice(index, 1);
+    localStorage.setItem('products', JSON.stringify(returnLocal));
+    localStorage.setItem('deleteProducts', JSON.stringify(blabla));
+}
 
 links.addEventListener('click', event => {
     for(item of items.children) {
@@ -25,29 +73,8 @@ links.addEventListener('click', event => {
     }
 });
 
-headerButton.addEventListener('click', event => {
-    newShoppingItem = document.createElement('li');
-    counter++;
-    shoppingList.append(newShoppingItem);
-    newShoppingItem.className = 'list__shoppingItem';
-    newShoppingItem.innerHTML = `
-        <span class="list__number">${counter}</span>
-        <span class="list__name">milk</span>
-        <span class="list__quantity">3</span>
-        <span class="list__price">2</span>
-        <div class="list__button">
-            <button class="list__doneButton">V</button>
-            <button class="list__editButton">E</button>
-            <button class="list__deleteButton">X</button>
-        </div>
-    `;
+headerButton.addEventListener('click', () => {
     addToLocalStorage();
+    showShoppingList();
 });
 
-const addToLocalStorage = function() {
-    itemObj.item = inputProduct.value;
-    itemObj.quantity = inputQuantity.value;
-    itemObj.price = inputPrice.value;
-    const productObj = JSON.stringify(itemObj);
-    localStorage.setItem(counter, productObj);
-}
