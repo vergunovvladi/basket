c4baa95282aecec82750ea0557b08abc48982bba
vladislav <vladislav.vergunov@icloud.com>
2021-06-18 12:44:14 +0300

    lol

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const links = document.querySelector('.links'),
       inputQuantity = document.querySelector('.header__quantity'),
       inputPrice = document.querySelector('.header__price'),
       shoppingList = document.querySelector('.list__shopping'),
-      listDelete = document.querySelector('.list__delete');
+      listDelete = document.querySelector('.list__delete'),
+      listDone = document.querySelector('.list__done');
 
 
 const addToProductLocal = function() {
@@ -42,26 +43,41 @@ const addToDeleteLocal = function(index) {
     showDeleteList();
 }
 
+const addToDoneLocal = function(index) {
+    const doneArr = [],
+          returnDoneLocal = JSON.parse(localStorage.getItem('doneProducts')),
+          returnLocal = JSON.parse(localStorage.getItem('products'));
+    if(returnDoneLocal !== null) {
+        for(i = 0; i < returnDoneLocal.length; i++) {
+            doneArr.push(returnDoneLocal[i]);
+        }
+    }
+    const doneItem = returnLocal.splice(index, 1);
+    localStorage.setItem('products', JSON.stringify(returnLocal));
+    doneArr.push(doneItem[0]);
+    localStorage.setItem('doneProducts', JSON.stringify(doneArr));
+}
+
 const showShoppingList = function() {
     const returnLocal = JSON.parse(localStorage.getItem('products'));
     if(returnLocal !== null) {
         shoppingList.innerHTML = '';
-    }
-    for(let i = 0; i < returnLocal.length; i++) {
-        let newShoppingItem = document.createElement('li');
-        shoppingList.append(newShoppingItem);
-        newShoppingItem.className = 'list__shoppingItem';
-        newShoppingItem.innerHTML = `
-        <span class="list__number">${i+1}</span>
-        <span class="list__name">${returnLocal[i].item}</span>
-        <span class="list__quantity">${returnLocal[i].quantity}</span>
-        <span class="list__price">${returnLocal[i].price}</span>
-        <div class="list__button">
-            <button class="list__doneButton">V</button>
-            <button class="list__editButton">E</button>
-            <button class="list__deleteButton">X</button>
-        </div>
-        `;
+        for(let i = 0; i < returnLocal.length; i++) {
+            let newShoppingItem = document.createElement('li');
+            shoppingList.append(newShoppingItem);
+            newShoppingItem.className = 'list__shoppingItem';
+            newShoppingItem.innerHTML = `
+            <span class="list__number">${i+1}</span>
+            <span class="list__name">${returnLocal[i].item}</span>
+            <span class="list__quantity">${returnLocal[i].quantity}</span>
+            <span class="list__price">${returnLocal[i].price}</span>
+            <div class="list__button">
+                <button class="list__doneButton">V</button>
+                <button class="list__editButton">E</button>
+                <button class="list__deleteButton">X</button>
+            </div>
+            `;
+        }
     }
 }
 
@@ -69,34 +85,73 @@ const showDeleteList = function () {
     const returnLocal = JSON.parse(localStorage.getItem('deleteProducts'));
     if(returnLocal != null) {
         listDelete.innerHTML = '';
+        for(let i = 0; i < returnLocal.length; i++) {
+            let newDeleteItem = document.createElement('li');
+            listDelete.append(newDeleteItem);
+            newDeleteItem.className = 'list__deleteItem';
+            newDeleteItem.innerHTML = `
+            <span class="list__number">${i+1}</span>
+            <span class="list__name">${returnLocal[i].item}</span>
+            <span class="list__quantity">${returnLocal[i].quantity}</span>
+            <span class="list__price">${returnLocal[i].price}</span>
+            <div class="list__button deleteRight">
+                <button class="list__deleteButton">X</button>
+            </div>
+            `;
+        }
     }
-    for(let i = 0; i < returnLocal.length; i++) {
-        let newDeleteItem = document.createElement('li');
-        listDelete.append(newDeleteItem);
-        newDeleteItem.className = 'list__deleteItem';
-        newDeleteItem.innerHTML = `
-        <span class="list__number">${i+1}</span>
-        <span class="list__name">${returnLocal[i].item}</span>
-        <span class="list__quantity">${returnLocal[i].quantity}</span>
-        <span class="list__price">${returnLocal[i].price}</span>
-        <div class="list__button">
-            <button class="list__doneButton">V</button>
-            <button class="list__editButton">E</button>
-            <button class="list__deleteButton">X</button>
-        </div>
-        `;
+}
+
+const showDoneList = function() {
+    const returnLocal = JSON.parse(localStorage.getItem('doneProducts'));
+    if(returnLocal != null) {
+        listDone.innerHTML = '';
+        for(let i = 0; i < returnLocal.length; i++) {
+            const newDoneItem = document.createElement('li');
+            listDone.append(newDoneItem);
+            newDoneItem.className = 'list__doneItem';
+            newDoneItem.innerHTML = `
+            <span class="list__number">${i+1}</span>
+            <span class="list__name">${returnLocal[i].item}</span>
+            <span class="list__quantity">${returnLocal[i].quantity}</span>
+            <span class="list__price">${returnLocal[i].price}</span>
+            <div class="list__button deleteRight">
+                <button class="list__deleteButton">X</button>
+            </div>
+            `;
+        }
     }
+}
+
+const clearInput = function() {
+    inputProduct.value = '';
+    inputQuantity.value = '';
+    inputPrice.value = '';
 }
 
 showShoppingList();
 showDeleteList();
+showDoneList();
 
 shoppingList.addEventListener('click', (event) => {
-    const deleteTarget = event.target.closest('li'),
-        deleteItem = deleteTarget.querySelector('.list__number'),
-        indexItem = parseInt(deleteItem.innerHTML);
+    if(event.target.className == 'list__deleteButton') {
+        const deleteTarget = event.target.closest('li'),
+            deleteItem = deleteTarget.querySelector('.list__number'),
+            indexItem = parseInt(deleteItem.innerHTML);
         addToDeleteLocal(indexItem - 1);
         showShoppingList();
+    }
+    if(event.target.className == 'list__doneButton') {
+        const doneTarget = event.target.closest('li'),
+              doneItem = doneTarget.querySelector('.list__number'),
+              indexDone = parseInt(doneItem.innerHTML);
+        addToDoneLocal(indexDone - 1);
+        showShoppingList();
+        showDoneList();
+    }
+    if(event.target.className == 'list__editButton') {
+
+    }
 });
 
 links.addEventListener('click', (event) => {
@@ -114,4 +169,27 @@ headerButton.addEventListener('click', () => {
     addToProductLocal();
     showShoppingList();
     showDeleteList();
+    clearInput();
 });
+
+listDelete.addEventListener('click', (event) => {
+    if(event.target.className == 'list__deleteButton') {
+        const returnLocal = JSON.parse(localStorage.getItem('deleteProducts'));
+              index = event.target.closest('li').querySelector('.list__number').innerHTML;
+        returnLocal.splice(index - 1);
+        localStorage.setItem('deleteProducts', JSON.stringify(returnLocal));
+    }
+    showDeleteList();
+});
+
+listDone.addEventListener('click', (event) => {
+    if(event.target.className == 'list__deleteButton') {
+        const returnLocal = JSON.parse(localStorage.getItem('doneProducts'));
+              index = event.target.closest('li').querySelector('.list__number').innerHTML;
+        returnLocal.splice(index - 1);
+        localStorage.setItem('doneProducts', JSON.stringify(returnLocal));
+    }
+    showDoneList();
+});
+
+// todo list edit
